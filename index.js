75737ca143100ee519728f905a9f44c8eecb62f9
lephuong <lenguyenphuong226@gmail.com>
2025-12-04 17:54:19 -0800

    build production, connect part 2 front end with backend part 3

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 
+const cors = require('cors')
 app.use(cors())
+
+app.use(express.static('dist'))
+
 const morgan = require('morgan')
 
 let persons = [
